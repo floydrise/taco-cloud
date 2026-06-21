bc93bacef74dcee68a18887bcd4c2cef6950fcd2
Stefan <floydrise@gmail.com>
2026-06-21 19:15:09 +0300

    Implement IngredientRepository and jdbc ingredient repository

---
+++ src/main/java/com/floydrise/tacocloud/tacos/data/IngredientRepository.java
@@ -0,0 +1,13 @@
+package com.floydrise.tacocloud.tacos.data;
+
+import java.util.Optional;
+
+import com.floydrise.tacocloud.tacos.attributes.Ingredient;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
