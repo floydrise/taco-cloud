d8ac153d96b4e55c7e6808831d51ad64ac1b8833
Stefan <floydrise@gmail.com>
2026-06-29 22:45:08 +0300

    Create many to one relation in TacoOrder with User
    Create relation and prefill user + user delivery details for the order

--- src/main/java/com/floydrise/tacocloud/tacos/attributes/TacoOrder.java
+++ src/main/java/com/floydrise/tacocloud/tacos/attributes/TacoOrder.java
@@ -9,19 +9,21 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
 
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 @Data
 @Entity
+@Table(name = "Taco_Order")
 public class TacoOrder implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -54,6 +56,9 @@ public class TacoOrder implements Serializable {
 
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+
+    @ManyToOne
+    private User user;
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
--- src/main/java/com/floydrise/tacocloud/tacos/web/OrderController.java
+++ src/main/java/com/floydrise/tacocloud/tacos/web/OrderController.java
@@ -3,15 +3,18 @@ package com.floydrise.tacocloud.tacos.web;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.floydrise.tacocloud.tacos.attributes.TacoOrder;
+import com.floydrise.tacocloud.tacos.attributes.User;
 import com.floydrise.tacocloud.tacos.data.OrderRepository;
 
 @Slf4j
@@ -26,16 +29,24 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String orderForm() {
+    public String orderForm(@ModelAttribute("tacoOrder") TacoOrder tacoOrder, @AuthenticationPrincipal User user) {
+        tacoOrder.setDeliveryName(user.getFullname());
+        tacoOrder.setDeliveryCity(user.getCity());
+        tacoOrder.setDeliveryState(user.getState());
+        tacoOrder.setDeliveryStreet(user.getStreet());
+        tacoOrder.setDeliveryZip(user.getZip());
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors, SessionStatus sessionStatus,
+            @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        tacoOrder.setUser(user);
 
+        System.out.println(user);
         orderRepo.save(tacoOrder);
         log.info("Processing taco order: {}", tacoOrder);
         sessionStatus.setComplete();
