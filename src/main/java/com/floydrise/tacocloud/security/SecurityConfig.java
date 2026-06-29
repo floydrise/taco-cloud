509b0495d94c2e31980c9f38caa667cf6cb3490b
Stefan <floydrise@gmail.com>
2026-06-29 22:02:24 +0300

    Add oauth2 and logout

--- src/main/java/com/floydrise/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/floydrise/tacocloud/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.floydrise.tacocloud.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.floydrise.tacocloud.tacos.attributes.User;
@@ -35,13 +42,26 @@ public class SecurityConfig {
         return http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/orders", "/design")
                 .hasRole("USER")
-                .requestMatchers("/", "/**")
+                .anyRequest()
                 .permitAll())
+                .oauth2Login(
+                        oauth2 -> oauth2.loginPage("/login").successHandler((request, response, authentication) -> {
+                            OAuth2User user = (OAuth2User) authentication.getPrincipal();
+                            String username = user.getAttribute("login");
+                            System.out.println(username);
+                            var authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+                            Authentication newAuth = new UsernamePasswordAuthenticationToken(user, null, authorities);
+                            SecurityContextHolder.getContext().setAuthentication(newAuth);
+                            response.sendRedirect("/design");
+                        }))
                 .formLogin((formLogin) -> formLogin
                         .usernameParameter("username")
                         .passwordParameter("password")
                         .loginPage("/login")
                         .defaultSuccessUrl("/design"))
+                .logout((logout) -> logout.deleteCookies("remove")
+                        .invalidateHttpSession(false)
+                        .logoutSuccessUrl("/"))
                 .build();
     }
 }
