48de9381668572895f3fdf4361ebdbec250da4ec
Stefan <floydrise@gmail.com>
2026-06-30 20:25:54 +0300

    Create configuration property holder
    Create the config property and display orders placed by a user ordered in desc by placed at

--- src/main/java/com/floydrise/tacocloud/tacos/data/OrderRepository.java
+++ src/main/java/com/floydrise/tacocloud/tacos/data/OrderRepository.java
@@ -1,12 +1,15 @@
 package com.floydrise.tacocloud.tacos.data;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.floydrise.tacocloud.tacos.attributes.TacoOrder;
+import com.floydrise.tacocloud.tacos.attributes.User;
 
 import java.util.List;
 
 public interface OrderRepository extends ListCrudRepository<TacoOrder, Long> {
     List<TacoOrder> findByDeliveryZip(String deliveryZip);
 
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
--- src/main/java/com/floydrise/tacocloud/tacos/web/OrderController.java
+++ src/main/java/com/floydrise/tacocloud/tacos/web/OrderController.java
@@ -3,8 +3,12 @@ package com.floydrise.tacocloud.tacos.web;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,11 +25,14 @@ import com.floydrise.tacocloud.tacos.data.OrderRepository;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
     private OrderRepository orderRepo;
+    private OrderProps orderProps;
 
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, OrderProps orderProps) {
         this.orderRepo = orderRepository;
+        this.orderProps = orderProps;
     }
 
     @GetMapping("/current")
@@ -36,6 +43,13 @@ public class OrderController {
         tacoOrder.setDeliveryStreet(user.getStreet());
         tacoOrder.setDeliveryZip(user.getZip());
         return "orderForm";
+    }
+
+    @GetMapping("/order_list")
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 
     @PostMapping
@@ -50,6 +64,6 @@ public class OrderController {
         orderRepo.save(tacoOrder);
         log.info("Processing taco order: {}", tacoOrder);
         sessionStatus.setComplete();
-        return "redirect:/";
+        return "redirect:/orders/order_list";
     }
 }
