0f15144f739f22ab0ff6422437e46e00b5d8abe8
Stefan <floydrise@gmail.com>
2026-07-07 16:40:28 +0300

    Create ingredients API endpoint
    Handle fetching, creating and deleting ingredients

---
+++ src/main/java/com/floydrise/tacocloud/tacos/web/api/IngredientController.java
@@ -0,0 +1,46 @@
+package com.floydrise.tacocloud.tacos.web.api;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.floydrise.tacocloud.tacos.attributes.Ingredient;
+import com.floydrise.tacocloud.tacos.data.IngredientRepository;
+
+@RestController
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin("http://localhost:8080")
+public class IngredientController {
+    private IngredientRepository ingredientRepository;
+
+    public IngredientController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping
+    public List<Ingredient> allIngredients() {
+        return ingredientRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientRepository.save(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@PathVariable("id") String ingredientId) {
+        ingredientRepository.deleteById(ingredientId);
+    }
+
+}
